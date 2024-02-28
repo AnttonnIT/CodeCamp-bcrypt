28f2fdd4ce0f86103c93403779e4b54887b003c0
Anton Kyryliuk <anttonnit@gmail.com>
2024-02-28 14:40:58 +0200

    end

--- server.js
+++ server.js
@@ -1,55 +1,34 @@
-'use strict';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const app         = express();
+"use strict";
+const express = require("express");
+const bodyParser = require("body-parser");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const app = express();
 fccTesting(app);
 const saltRounds = 12;
-const myPlaintextPassword = 'sUperpassw0rd!';
-const someOtherPlaintextPassword = 'pass123';
-
+const myPlaintextPassword = "sUperpassw0rd!";
+const someOtherPlaintextPassword = "pass123";
+const bcrypt = require("bcrypt");
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  /*Store hash in your db*/
+  console.log(hash);
 
-
-
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res);
+    /*res == true or false*/
+  });
+});
 //END_ASYNC
 
 //START_SYNC
-
-
-
+const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+const result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 //END_SYNC
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log("Listening on port:", PORT)
+  console.log("Listening on port:", PORT);
 });
